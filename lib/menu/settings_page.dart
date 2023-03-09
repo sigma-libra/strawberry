30f7d5e5c2f86b161de280a25ed577ff4dcd6892
sigma-libra <sabrina.ckall@gmail.com>
2023-03-09 23:06:20 +0100

    control averages through controllers

--- lib/menu/settings_page.dart
+++ lib/menu/settings_page.dart
@@ -19,6 +19,25 @@ class SettingsPage extends StatefulWidget {
 }
 
 class SettingsPageState extends State<SettingsPage> {
+
+   TextEditingController _cycleController = TextEditingController();
+   TextEditingController _periodController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _cycleController = TextEditingController(text: widget.configs.getInt(AVERAGE_CYCLE_KEY).toString());
+    _periodController = TextEditingController(text: widget.configs.getInt(AVERAGE_PERIOD_KEY).toString());
+  }
+
+  @override
+  void dispose() {
+    _cycleController.dispose();
+    _periodController.dispose();
+    super.dispose();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,26 +54,25 @@ class SettingsPageState extends State<SettingsPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
-              initialValue: widget.configs.getInt(AVERAGE_CYCLE_KEY).toString(),
+              controller: _cycleController,
               decoration: const InputDecoration(labelText: "Cycle Duration"),
               keyboardType: TextInputType.number,
               inputFormatters: <TextInputFormatter>[
                 FilteringTextInputFormatter.digitsOnly
               ],
-              onChanged: _setCycle, // Only numbers can be entered
             ),
             TextFormField(
-              initialValue:
-                  widget.configs.getInt(AVERAGE_PERIOD_KEY).toString(),
+              controller: _periodController,
               decoration: const InputDecoration(labelText: "Period Duration"),
               keyboardType: TextInputType.number,
               inputFormatters: <TextInputFormatter>[
                 FilteringTextInputFormatter.digitsOnly
               ],
-              onChanged: _setPeriod, // Only numbers can be entered
             ),
             ElevatedButton(
               onPressed: () {
+                _setPeriod(_periodController.value.text);
+                _setCycle(_cycleController.value.text);
                 Navigator.pop(context);
               },
               child: const Text('Save'),
